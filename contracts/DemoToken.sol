332c9b98ca0e70a4f723c443c85cd9690de7fab5
alenabelium <33514602+alenabelium@users.noreply.github.com>
2024-11-09 19:46:13 +0100

    token code

--- contracts/DemoToken.sol
+++ contracts/DemoToken.sol
@@ -1,1 +1,13 @@
-//
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+contract DemoToken is ERC20 {
+    constructor(
+        string memory_name,
+        string memory _symbol
+    ) ERC20(_name, _symbol) {
+        _mint(msg.sender, 1000 * 10 ** 18);
+    }
+}
